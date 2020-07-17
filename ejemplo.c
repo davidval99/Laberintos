c1b74a8490b523f7e741b701e83473a0610aa73a
David Umaña Blanco <davidumana7@gmail.com>
2020-07-16 23:41:03 -0600

    Update ejemplo.c
    Creacion de tablero para el laberinto

--- ejemplo.c
+++ ejemplo.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <GL/glut.h>
 //#include SOIL.h
+int FILAS;
+int COLUMNAS;
+
 
 int c = 0;
 void init()
@@ -12,16 +15,15 @@ void init()
     // Choosing the type of projection
     glMatrixMode(GL_PROJECTION);
     // for setting the transformation which here is 2D
-    gluOrtho2D(0, 800, 0, 600);
+    gluOrtho2D(0, COLUMNAS, 0, FILAS);//columnas y filas
 }
 
-void drawSquare(GLint x1, GLint y1, GLint x2, GLint y2, GLint x3, GLint y3, GLint x4, GLint y4)
+void drawSquare(GLint x1, GLint y1, GLint x2, GLint y2, GLint y3)
 {
     
     // if color is 1 then draw black box and change value of color = 0
   
     glColor3f(0, 0, 0); // black color value is 0 0 0
-    c = 0;
     
     // Draw Square
     glBegin(GL_LINES);
@@ -32,31 +34,63 @@ void drawSquare(GLint x1, GLint y1, GLint x2, GLint y2, GLint x3, GLint y3, GLin
     glBegin(GL_LINES);
     glVertex2i(x2, y2);
     glVertex2i(x2, y3);
+    glEnd();
 
     //glBegin(GL_LINES);
     //glVertex2i(x1, y3);
     //glVertex2i(x4, y4);
-    glEnd();
+    //glEnd();
 }
-void chessboard()
+
+
+
+//void maze(int filas, int columnas)
+void mazeboard()
 {
     glClear(GL_COLOR_BUFFER_BIT); // Clear display window
-    GLint x,y;
+    GLint x, y;
 
-    for (x = 0; x <= 800; x += 100)
+    for (x = 0; x < COLUMNAS; x += 100)
     {
-        for (y = 0; y <= 600; y += 75)
+        for (y = 0; y < FILAS; y+= 100)
         {
-            drawSquare(x, y + 75, x + 100, y + 75, x + 100, y, x, y);
+            drawSquare(x, y, x+100, y, y+100);
         
         }
     }
+
+
+    glColor3f(1,0,0);
+    
+    glBegin(GL_LINES);
+    glVertex2i(0,0);
+    glVertex2i(0, FILAS);
+    glEnd();
+
+
+    glColor3f(1,0,0);
+    
+    glBegin(GL_LINES);
+    glVertex2i(0,FILAS);
+    glVertex2i(COLUMNAS, FILAS);
+    glEnd();
+
+    /*
+    glColor3f(1,1,1); // white color
+
+    glBegin(GL_LINES);
+    glVertex2i(200,100);
+    glVertex2i(300, 100);
+    glEnd();*/
+
     // Process all OpenGL routine s as quickly as possible
     glFlush();
 }
 
 int main(int agrc, char **argv)
 {
+    FILAS = 1200;
+    COLUMNAS = 1200;
     // Initialize GLUT
     glutInit(&agrc, argv);
     // Set display mode
@@ -64,13 +98,13 @@ int main(int agrc, char **argv)
     // Set top - left display window position.
     glutInitWindowPosition(100, 100);
     // Set display window width and height
-    glutInitWindowSize(800, 600);
+    glutInitWindowSize(5000, 5000);
     // Create display window with the given title
-    glutCreateWindow("Chess Board using OpenGL in C++");
+    glutCreateWindow("Maze solver using OpenGL in C");
     // Execute initialization procedure
     init();
     // Send graphics to display window
-    glutDisplayFunc(chessboard);
+    glutDisplayFunc(mazeboard);
     // Display everything and wait.
     glutMainLoop();
 }
